f11756dc63b696263eb53a1b6adb5dbd7786a69c
jokatty <jyotikattani@gmail.com>
2021-05-05 11:25:15 +0800

    fb branch inital commit

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 let inputName = process.argv[2];
 console.log(`hello ${inputName.toUpperCase()}! How are you?`);
+
+import { writeFile } from 'fs';
+let name = process.argv[2];
+writeFile('data.json', name, (err) => console.error('err ->', err));
